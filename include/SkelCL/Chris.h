cf68957494adc9659e527c2f813d721ce6a21980
Chris Cummins <chrisc.101@gmail.com>
2015-01-28 15:20:28 +0000

    Add a "private" header file for my own work
    This adds a "Chris.h" header file which will help segregate my work from
    the main SkelCL codebase.

---
+++ include/SkelCL/Chris.h
@@ -0,0 +1,30 @@
+///
+/// \file Chris.h
+///
+///	\author Chris Cummins <chrisc.101@gmail.com>
+///
+/// This file is used to help segregate Chris' work from the main
+/// SkelCL codebase, and contains code revelant to his work on
+/// autotuning SkelCL.
+
+#ifndef CHRIS_H_
+#define CHRIS_H_
+
+namespace chris {
+
+
+///
+/// Return the number of partitions to use for a given inputBuffer and
+/// work group size.
+///
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+template <typename Tin>
+cl_uint get_num_partitions(Tin &inputBuffer, const cl_uint workGroupSize) {
+  return static_cast<cl_uint>(2);
+}
+#pragma GCC diagnostic pop //  -Wunused-parameter
+
+
+} // namespace chris
+
+#endif // CHRIS_H_
