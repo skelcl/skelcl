0ce9f4ba29275b8831e9fc71147b46ca7f030377
Chris Cummins <chrisc.101@gmail.com>
2015-03-09 11:16:37 +0000

    Revert "Add #define for OpenCL compiler flags"
    This reverts commit b7171d5e1a9abfaa08dbd1d07dcdd6b0758a8b5c.

--- include/SkelCL/Chris.h
+++ include/SkelCL/Chris.h
@@ -10,8 +10,6 @@
 #ifndef CHRIS_H_
 #define CHRIS_H_
 
-#define CHRIS_OPENCL_FLAG "-cl-fast-relaxed-math"
-
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Weffc++"
 #include <CL/cl.h>
