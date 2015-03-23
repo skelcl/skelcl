eaaf3ffce3e9f341f5b8902f8f9b53c9b7523048
Chris Cummins <chrisc.101@gmail.com>
2015-03-23 20:41:24 +0000

    StencilInfoDef: Whitespace and alignment

--- include/SkelCL/detail/StencilInfoDef.h
+++ include/SkelCL/detail/StencilInfoDef.h
@@ -12,32 +12,32 @@ namespace skelcl {
 
 template<typename Tin, typename Tout>
 StencilInfo<Tout(Tin)>::StencilInfo(const Source& source, unsigned int north,
-		unsigned int west, unsigned int south, unsigned int east,
-		detail::Padding padding, Tin neutral_element, const std::string& func) :
-		_userSource(source), _north(north), _west(west), _south(south), _east(
-				east), _padding(padding), _neutral_element(neutral_element), _funcName(
-				func), _program(createAndBuildProgram()) {
-	LOG_DEBUG_INFO("Create new StencilInfo object (", this, ")");
+                                    unsigned int west, unsigned int south, unsigned int east,
+                                    detail::Padding padding, Tin neutral_element, const std::string& func) :
+  _userSource(source), _north(north), _west(west), _south(south), _east(east),
+      _padding(padding), _neutral_element(neutral_element), _funcName(
+          func), _program(createAndBuildProgram()) {
+    LOG_DEBUG_INFO("Create new StencilInfo object (", this, ")");
 }
 
 template<typename Tin, typename Tout>
 detail::Program StencilInfo<Tout(Tin)>::createAndBuildProgram() const {
-	ASSERT_MESSAGE(!_userSource.empty(),
-			"Tried to create program with empty user source.");
+  ASSERT_MESSAGE(!_userSource.empty(),
+                 "Tried to create program with empty user source.");
 
-	std::stringstream temp;
+  std::stringstream temp;
 
-	//Determine the padding mode
-	switch (_padding) {
-	case detail::Padding::NEUTRAL: {
-		temp << "#define NEUTRAL " << _neutral_element << "\n";
-	}
-		break;
+  //Determine the padding mode
+  switch (_padding) {
+    case detail::Padding::NEUTRAL: {
+      temp << "#define NEUTRAL " << _neutral_element << "\n";
+    }
+      break;
 
-	case detail::Padding::NEAREST:
-	case detail::Padding::NEAREST_INITIAL:
-		break;
-	}
+    case detail::Padding::NEAREST:
+    case detail::Padding::NEAREST_INITIAL:
+      break;
+  }
 
   temp << "#define SCL_NORTH (" << _north << ")\n"
        << "#define SCL_WEST  (" << _west  << ")\n"
@@ -54,14 +54,14 @@ detail::Program StencilInfo<Tout(Tin)>::createAndBuildProgram() const {
        << "#define SCL_L_ID (SCL_L_ROW * SCL_L_COL_COUNT + SCL_L_COL)\n"
        << "#define SCL_ROWS (SCL_ELEMENTS / SCL_COLS)\n";
 
-	// create program
+  // create program
   std::string s(detail::CommonDefinitions::getSource());
-	s.append(Matrix<Tout>::deviceFunctions());
-	s.append(temp.str());
+  s.append(Matrix<Tout>::deviceFunctions());
+  s.append(temp.str());
 
-	if (_north == 0 && _south == 0 && _west == 0 && _east == 0) {
-		s.append(
-				R"(
+  if (_north == 0 && _south == 0 && _west == 0 && _east == 0) {
+    s.append(
+        R"(
 
     typedef float SCL_TYPE_0;
     typedef float SCL_TYPE_1;
@@ -76,11 +76,10 @@ detail::Program StencilInfo<Tout(Tin)>::createAndBuildProgram() const {
     }
 
 )");
-	} else {
-
-		// helper structs and functions
-		s.append(
-				R"(
+  } else {
+    // helper structs and functions
+    s.append(
+        R"(
 
 typedef float SCL_TYPE_0;
 typedef float SCL_TYPE_1;
@@ -99,12 +98,12 @@ SCL_TYPE_1 getData(input_matrix_t* matrix, int x, int y){
 }
 
 )");
-	}
-	// user source
-	s.append(_userSource);
-	if (_north == 0 && _south == 0 && _west == 0 && _east == 0) {
-		s.append(
-				R"(
+  }
+  // user source
+  s.append(_userSource);
+  if (_north == 0 && _south == 0 && _west == 0 && _east == 0) {
+    s.append(
+        R"(
 
 __kernel void SCL_STENCIL(__global SCL_TYPE_0* SCL_IN,
                           __global SCL_TYPE_1* SCL_OUT,
@@ -120,105 +119,103 @@ if (get_global_id(1)*SCL_COLS+get_global_id(0) < SCL_ELEMENTS) {
 }
 }
 )");
-	} else {
-		if (_padding == detail::Padding::NEAREST)
-			s.append(
+  } else {
+    // Build the kernel.
+    if (_padding == detail::Padding::NEAREST)
+      s.append(
 #include "StencilKernelNearest.cl"
 			);
-
-		else if (_padding == detail::Padding::NEUTRAL) {
-			s.append(
+    else if (_padding == detail::Padding::NEUTRAL) {
+      s.append(
 #include "StencilKernelNeutral.cl"
 			);
-		}
-
-		else if (_padding == detail::Padding::NEAREST_INITIAL) {
-			s.append(
+    }
+    else if (_padding == detail::Padding::NEAREST_INITIAL) {
+      s.append(
 #include "StencilKernelNearestInitial.cl"
 			);
-		}
-	}
-	auto program = detail::Program(s,
-			detail::util::hash(
-					"//Stencil\n" + Matrix<Tout>::deviceFunctions()
-							+ _userSource + _funcName));
-	// modify program
-	if (!program.loadBinary()) {
-		program.transferParameters(_funcName, 1, "SCL_STENCIL");
-		program.transferArguments(_funcName, 1, "USR_FUNC");
-		program.renameFunction(_funcName, "USR_FUNC");
-		program.adjustTypes<Tin, Tout>();
-	}
-	program.build();
-	return program;
+    }
+  }
+  auto program = detail::Program(s,
+                                 detail::util::hash(
+                                     "//Stencil\n" + Matrix<Tout>::deviceFunctions()
+                                     + _userSource + _funcName));
+  // modify program
+  if (!program.loadBinary()) {
+    program.transferParameters(_funcName, 1, "SCL_STENCIL");
+    program.transferArguments(_funcName, 1, "USR_FUNC");
+    program.renameFunction(_funcName, "USR_FUNC");
+    program.adjustTypes<Tin, Tout>();
+  }
+  program.build();
+  return program;
 }
 
 template<typename Tin, typename Tout>
 unsigned int StencilInfo<Tout(Tin)>::determineMaxWorkGroupSize() const {
-	unsigned int maxWorkgroupSize = INT_MAX;
-	for (auto iter = detail::globalDeviceList.begin();
-			iter != detail::globalDeviceList.end(); ++iter) {
-		if (iter->get()->maxWorkGroupSize() < maxWorkgroupSize) {
-			maxWorkgroupSize = iter->get()->maxWorkGroupSize();
-		}
-	}
-	return maxWorkgroupSize;
+  unsigned int maxWorkgroupSize = INT_MAX;
+  for (auto iter = detail::globalDeviceList.begin();
+       iter != detail::globalDeviceList.end(); ++iter) {
+    if (iter->get()->maxWorkGroupSize() < maxWorkgroupSize) {
+      maxWorkgroupSize = iter->get()->maxWorkGroupSize();
+    }
+  }
+  return maxWorkgroupSize;
 }
 
 template<typename Tin, typename Tout>
 unsigned int StencilInfo<Tout(Tin)>::determineTileWidth() const {
-	return sqrt(determineMaxWorkGroupSize()) + _west + _east;
+  return sqrt(determineMaxWorkGroupSize()) + _west + _east;
 }
 
 template<typename Tin, typename Tout>
 unsigned int StencilInfo<Tout(Tin)>::determineTileHeight() const {
-	return sqrt(determineMaxWorkGroupSize()) + _south + _north;
+  return sqrt(determineMaxWorkGroupSize()) + _south + _north;
 }
 
 template<typename Tin, typename Tout>
-const unsigned int& StencilInfo<Tout(Tin)>::getNorth() const {
-	return this->_north;
+    const unsigned int& StencilInfo<Tout(Tin)>::getNorth() const {
+  return this->_north;
 }
 
 template<typename Tin, typename Tout>
 const unsigned int& StencilInfo<Tout(Tin)>::getWest() const {
-	return this->_west;
+  return this->_west;
 }
 
 template<typename Tin, typename Tout>
 const unsigned int& StencilInfo<Tout(Tin)>::getSouth() const {
-	return this->_south;
+  return this->_south;
 }
 
 template<typename Tin, typename Tout>
 const unsigned int& StencilInfo<Tout(Tin)>::getEast() const {
-	return this->_east;
+  return this->_east;
 }
 
 template<typename Tin, typename Tout>
 const detail::Padding& StencilInfo<Tout(Tin)>::getPadding() const {
-	return this->_padding;
+  return this->_padding;
 }
 
 template<typename Tin, typename Tout>
 const Tin& StencilInfo<Tout(Tin)>::getNeutralElement() const {
-	return this->_neutral_element;
+  return this->_neutral_element;
 }
 
 template<typename Tin, typename Tout>
 const detail::Program& StencilInfo<Tout(Tin)>::getProgram() const {
-	return this->_program;
+  return this->_program;
 }
 
 template<typename Tin, typename Tout>
 std::string StencilInfo<Tout(Tin)>::getDebugInfo() const {
-	std::stringstream s;
-	s << "north: " << _north << ", west: " << _west << ", south: " << _south
-			<< ", east: " << _east;
-	return s.str();
+  std::stringstream s;
+  s << "north: " << _north << ", west: " << _west << ", south: " << _south
+    << ", east: " << _east;
+  return s.str();
 }
 
-}
+}  // namespace
 
-#endif /* STENCILINFODEF_H_ */
-
+#endif  /* STENCILINFODEF_H_ */
